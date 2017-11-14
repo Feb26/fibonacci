7475453c637fc33cae79ca06e8074fa8fc652fc4
by26feb <cha202802@gmail.com>
2017-11-14 23:10:42 +0000

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const length = 40;
+
+function fib(n) {
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } else {
+        return fib(n - 2) + fib(n - 1);
+    }
+}
+
+for (let i = 0; i <= length; i++) {
+    console.log(fib(i));
+}
